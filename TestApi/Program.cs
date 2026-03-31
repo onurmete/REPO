4883c2cd5e2024b7a9d4e22f1bddf181eca682a7
onurmete <onurm90@gmail.com>
2026-04-01 01:16:35 +0300

    Refactor health check endpoint to single line

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -1,10 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/health", () => new
-{
-    status = "ok",
-    version = "1.0.0"
-});
+app.MapGet("/health", () => new { status = "ok", version = "1.0.0" });
 
 app.Run("http://0.0.0.0:8080");
